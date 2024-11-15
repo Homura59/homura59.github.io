0beb6b2d7fc834ff5ac5928b112e986fd964fde8
Akemi Homura <40329992+Homura59@users.noreply.github.com>
2024-11-15 20:23:48 +0800

    Update ohmylive2d.js

--- assets/js/ohmylive2d.js
+++ assets/js/ohmylive2d.js
@@ -1,15 +1,15 @@
 <script src="https://unpkg.com/oh-my-live2d@latest"></script>
-<script>
-  OML2D.loadOml2d({
-    models: [
-      {
-        path: 'https://model.oml2d.com/HK416-1-normal/model.json',
-        position: [0, 60],
-        scale: 0.08,
-        stageStyle: {
-          height: 450
-        }
+
+OML2D.loadOml2d({
+  models: [
+    {
+      path: 'https://model.oml2d.com/HK416-1-normal/model.json',
+      position: [0, 60],
+      scale: 0.08,
+      stageStyle: {
+        height: 450
       }
-    ]
-  });
-</script>
+    }
+  ]
+});
+
